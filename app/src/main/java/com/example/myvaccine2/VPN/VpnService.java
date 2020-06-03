74e8cdb449f1e898f4ae9de60b2da96cf2b3f864
ggj0418 <ggj0418@naver.com>
2020-06-04 05:42:17 +0900

    Local VPN 구동 부분 추가 VPN 구동 시, notification 팝업 추가

--- app/src/main/java/com/example/myvaccine2/VPN/VpnService.java
+++ app/src/main/java/com/example/myvaccine2/VPN/VpnService.java
@@ -1,88 +1,213 @@
 package com.example.myvaccine2.VPN;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.example.myvaccine2.R;
+
+import java.io.Closeable;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class VpnService extends android.net.VpnService {
-    private Thread mThread;
-    private ParcelFileDescriptor mInterface;
-    // 인터페이스를 위한 builder 객체 생성
-    Builder builder = new Builder();
+    private static final String TAG = VpnService.class.getSimpleName();
+    private static final String VPN_ADDRESS = "10.0.0.2";
+    private static final String VPN_ROUTE = "0.0.0.0";  // 모든 패킷 리스닝
 
-    // VPN 서비스 인터페이스
+    public static final String BROADCAST_VPN_STATE = "com.example.myvaccine2.VPN.VPN_STATE";
+
+    private static boolean isRunning = false;
+
+    private ParcelFileDescriptor vpnInterface = null;
+
+    private PendingIntent pendingIntent;
+
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+    private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+    private ExecutorService executorService;
+
+    private Selector udpSelector;
+    private Selector tcpSelector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        isRunning = true;
+        setupVPN();
+
+        try {
+            udpSelector = Selector.open();
+            tcpSelector = Selector.open();
+            deviceToNetworkUDPQueue = new ConcurrentLinkedQueue<>();
+            deviceToNetworkTCPQueue = new ConcurrentLinkedQueue<>();
+            networkToDeviceQueue = new ConcurrentLinkedQueue<>();
+
+            executorService = Executors.newFixedThreadPool(5);
+            executorService.submit(new UDPInput(networkToDeviceQueue, udpSelector));
+            executorService.submit(new UDPOutput(deviceToNetworkUDPQueue, udpSelector, this));
+            executorService.submit(new TCPInput(networkToDeviceQueue, tcpSelector));
+            executorService.submit(new TCPOutput(deviceToNetworkTCPQueue, networkToDeviceQueue, tcpSelector, this));
+            executorService.submit(new VPNRunnable(vpnInterface.getFileDescriptor(), deviceToNetworkUDPQueue, deviceToNetworkTCPQueue, networkToDeviceQueue));
+            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(BROADCAST_VPN_STATE).putExtra("running", true));
+
+            Log.i(TAG, "Started");
+        } catch (IOException e) {
+            Log.e(TAG, "Error starting service", e);
+            cleanup();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isRunning = false;
+        executorService.shutdownNow();
+        cleanup();
+        Log.i(TAG, "Stopped");
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
 
-        // 새로운 스레드 생성으로 새로운 세션 시작
-        mThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    // 1. TUN 인터페이스 구성 (TUN이란 커널에서 제공하는 NIC를 직접 제어하지 않고 소프트웨어적으로 구현된 가상 네트워크 장치를 제어하는 가상 네트워크 드라이버)
-                    mInterface = builder.setSession("VPNService")
-                            .addAddress("192.168.0.1", 24)
-                            .addDnsServer("8.8.8.8")
-                            .addRoute("0.0.0.0", 0)
-                            .establish();
-                    // 2-1. 발신된 패킷들은 여기 inputStream에 축적
-                    FileInputStream in = new FileInputStream(mInterface.getFileDescriptor());
-                    // 2-2. 수신된 패킷들은 outputStream에 기록
-                    FileOutputStream out = new FileOutputStream(mInterface.getFileDescriptor());
-                    // 3. UDP 채널은 ip package를 서버로 보내거나 서버에서 받는 것에 사용
-                    DatagramChannel tunnel = DatagramChannel.open();
-                    // 서버에 연결하면 localhost는 오직 데모용으로만 사용
-                    // InetAddress.getlocalHost()를 127.0.0.1 대신 사용 가능
-                    tunnel.connect(new InetSocketAddress(InetAddress.getLocalHost(), 8087));
-                    // 4. 이 소켓을 보호하므로 패키지로 전송하면 VPN 서비스에 대한 피드백이 아님
-                    // 앱의 터널 소켓을 시스템 VPN 외부에 유지하고 순환 연결을 방지
-                    protect(tunnel.socket());
+    public static boolean isRunning() {
+        return isRunning;
+    }
 
-                    // 하나의 패킷에 대한 버퍼 할당
-                    ByteBuffer packet = ByteBuffer.allocate(32767);
-                    // 터널의 상태를 파악하기 위해 타이머를 사용
-                    // 입출력 둘다에서 작동
-                    // 양수값은 발신 중을 의미하며 다른 값들은 수신 중을 의미
-                    // 수신부터 시작
-                    int timer = 0;
-                    Thread.sleep(1000);
-                    // 문제가 발생하기 전까지 forward 상태 유지
-                    while(true) {
-                        // 첫 반복에서는 아무런 발생이 없다고 가정
-                        boolean idle = true;
-                        // inputStream에서 발신될 패킷을 read
-                        int length = in.read(packet.array());
-                        if (length > 0) {
-                            // tunnel에 발신될 패킷을 write
-                            packet.limit(length);
-                            DatagramPacket datagramPacket = new DatagramPacket(packet.array(), packet.array().length);
-                            byte[] dataInThePacket = datagramPacket.getData();
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if(mInterface != null) {
-                            mInterface.close();
-                            mInterface = null;
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+    private void setupVPN()
+    {
+        if (vpnInterface == null)
+        {
+            Builder builder = new Builder();
+            builder.addAddress(VPN_ADDRESS, 32);
+            builder.addRoute(VPN_ROUTE, 0);
+            vpnInterface = builder.setSession(getString(R.string.app_name)).setConfigureIntent(pendingIntent).establish();
+        }
+    }
+
+    private void cleanup()
+    {
+        deviceToNetworkTCPQueue = null;
+        deviceToNetworkUDPQueue = null;
+        networkToDeviceQueue = null;
+        ByteBufferPool.clear();
+        closeResources(udpSelector, tcpSelector, vpnInterface);
+    }
+
+    private static void closeResources(Closeable... resources) {
+        for (Closeable resource : resources) {
+            try {
+                resource.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }, "VpnRunnable");
+        }
+    }
 
-        return super.onStartCommand(intent, flags, startId);
+    private static class VPNRunnable implements Runnable
+    {
+        private static final String TAG = VPNRunnable.class.getSimpleName();
+
+        private FileDescriptor vpnFileDescriptor;
+
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+        private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+
+        public VPNRunnable(FileDescriptor vpnFileDescriptor,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue,
+                           ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue) {
+            this.vpnFileDescriptor = vpnFileDescriptor;
+            this.deviceToNetworkUDPQueue = deviceToNetworkUDPQueue;
+            this.deviceToNetworkTCPQueue = deviceToNetworkTCPQueue;
+            this.networkToDeviceQueue = networkToDeviceQueue;
+        }
+
+        @Override
+        public void run()
+        {
+            Log.i(TAG, "Started");
+
+            FileChannel vpnInput = new FileInputStream(vpnFileDescriptor).getChannel();
+            FileChannel vpnOutput = new FileOutputStream(vpnFileDescriptor).getChannel();
+
+            try
+            {
+                ByteBuffer bufferToNetwork = null;
+                boolean dataSent = true;
+                boolean dataReceived;
+                while (!Thread.interrupted())
+                {
+                    if (dataSent)
+                        bufferToNetwork = ByteBufferPool.acquire();
+                    else
+                        bufferToNetwork.clear();
+
+                    // TODO: Block when not connected
+                    int readBytes = vpnInput.read(bufferToNetwork);
+                    if (readBytes > 0) {
+                        dataSent = true;
+                        bufferToNetwork.flip();
+                        Packet packet = new Packet(bufferToNetwork);
+                        if (packet.isUDP()) {
+                            deviceToNetworkUDPQueue.offer(packet);
+                        } else if (packet.isTCP()) {
+                            deviceToNetworkTCPQueue.offer(packet);
+                        } else {
+                            Log.w(TAG, "Unknown packet type");
+                            Log.w(TAG, packet.ip4Header.toString());
+                            dataSent = false;
+                        }
+                    } else {
+                        dataSent = false;
+                    }
+
+                    ByteBuffer bufferFromNetwork = networkToDeviceQueue.poll();
+                    if (bufferFromNetwork != null)
+                    {
+                        bufferFromNetwork.flip();
+                        while (bufferFromNetwork.hasRemaining())
+                            vpnOutput.write(bufferFromNetwork);
+                        dataReceived = true;
+
+                        ByteBufferPool.release(bufferFromNetwork);
+                    } else {
+                        dataReceived = false;
+                    }
+
+                    // TODO: Sleep-looping is not very battery-friendly, consider blocking instead
+                    // Confirm if throughput with ConcurrentQueue is really higher compared to BlockingQueue
+                    if (!dataSent && !dataReceived)
+                        Thread.sleep(10);
+                }
+            } catch (InterruptedException e) {
+                Log.i(TAG, "Stopping");
+            } catch (IOException e) {
+                Log.w(TAG, e.toString(), e);
+            } finally {
+                closeResources(vpnInput, vpnOutput);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myvaccine2/VaccineReadyActivity.java
+++ app/src/main/java/com/example/myvaccine2/VaccineReadyActivity.java
@@ -1,8 +1,17 @@
 package com.example.myvaccine2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,6 +21,7 @@ import android.widget.Toast;
 
 import com.example.myvaccine2.API.APICallMethod;
 import com.example.myvaccine2.Privacy.Method;
+import com.example.myvaccine2.VPN.VpnService;
 
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -25,14 +35,38 @@ import io.reactivex.schedulers.Schedulers;
 // DB에서 웹 비콘 정보를 받은 후 이를 디바이스에 심는 액티비티
 public class VaccineReadyActivity extends AppCompatActivity {
 
-    Disposable disposable;
+    /*Disposable disposable;
     Context context;
 
     Button button;
     ImageView image1, image2, image3;
 
-    public List<String> beaconInfoList;
-    private static final int img1 = 0, img2 = 1, img3 = 2;
+    public List<String> beaconInfoList;*/
+
+    private static final int VPN_REQUEST_CODE = 0x0F;
+    private static final String NOTIFICATION_CHANNEL_ID = "10001";
+
+    private boolean waitingForVPNStart;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
+            waitingForVPNStart = true;
+            startService(new Intent(this, VpnService.class));
+            enableButton(false);
+        }
+    }
+
+    private BroadcastReceiver vpnStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (VpnService.BROADCAST_VPN_STATE.equals(intent.getAction())) {
+                if (intent.getBooleanExtra("running", false))
+                    waitingForVPNStart = false;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +74,87 @@ public class VaccineReadyActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_vaccine_ready);
 //        APICallMethod.findFromLog("beam_1_SMS.jpg", "beam_2_Call.jpg", "beam_3_Phonebook.jpg");
+
+        final Button button = (Button) findViewById(R.id.vra_vpn_start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVPN();
+                notificationSetting();
+            }
+        });
+        waitingForVPNStart = false;
+        LocalBroadcastManager.getInstance(this).registerReceiver(vpnStateReceiver, new IntentFilter(VpnService.BROADCAST_VPN_STATE));
     }
 
-    @Override
-    protected void onStart() {
-        context = this;
-        initSetting();
+    private void startVPN() {
+        Intent vpnIntent = VpnService.prepare(this);
+        if (vpnIntent != null)
+            startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
+        else
+            onActivityResult(VPN_REQUEST_CODE, RESULT_OK, null);
+    }
 
-        super.onStart();
+    private void enableButton(boolean enable) {
+        final Button vpnButton = (Button) findViewById(R.id.vra_vpn_start_button);
+        if (enable) {
+            vpnButton.setEnabled(true);
+            vpnButton.setText(R.string.start_vpn);
+        } else {
+            vpnButton.setEnabled(false);
+            vpnButton.setText(R.string.stop_vpn);
+        }
+    }
+
+    // VPN 연결 혹은 해제 시 notification 팝업
+    private void notificationSetting() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat
+                .Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground)) //BitMap 이미지 요구
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("VPN을 중지시키려면 터치하세요")
+                // 더 많은 내용이라서 일부만 보여줘야 하는 경우 아래 주석을 제거하면 setContentText에 있는 문자열 대신 아래 문자열을 보여줌
+                // .setStyle(new NotificationCompat.BigTextStyle().bigText("더 많은 내용을 보여줘야 하는 경우..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // .setContentIntent(pendingIntent) // 사용자가 노티피케이션을 탭시 ResultActivity로 이동하도록 설정
+                .setAutoCancel(true);
+
+        //OREO API 26 이상에서는 채널 필요
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setSmallIcon(R.drawable.ic_mine); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
+            CharSequence channelName  = "노티페케이션 채널";
+            String description = "오레오 이상을 위한 것임";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName , importance);
+            channel.setDescription(description);
+
+            // 노티피케이션 채널을 시스템에 등록
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        } else
+            builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
+
+        assert notificationManager != null;
+        notificationManager.notify(Integer.parseInt(NOTIFICATION_CHANNEL_ID), builder.build()); // 고유숫자로 노티피케이션 동작시킴
     }
 
     @Override
     protected void onResume() {
         // DB에서 받아온 비콘 정보를 리스트에 저장
-        beaconInfoList = APICallMethod.getBeaconInfo();
+//        beaconInfoList = APICallMethod.getBeaconInfo();
         super.onResume();
+        enableButton(!waitingForVPNStart && !VpnService.isRunning());
+    }
+}
+
+    /*@Override
+    protected void onStart() {
+        context = this;
+        initSetting();
+        super.onStart();
     }
 
     @Override
@@ -173,5 +273,4 @@ public class VaccineReadyActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "준비가 완료되었습니다", Toast.LENGTH_LONG).show();
                     }
                 });
-    }
-}
+    }*/
